63bb254899e086f4fdd9ce41d4abae249b4a311d
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-10-06 22:58:45 +0900

    2.7
    ssibal

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,63 @@
-hello;
+const socket = io();
+
+const welcom = document.getElementById("welcome");
+const form = welcom.querySelector("form");
+
+const room = document.getElementById("room");
+room.hidden = true;
+
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, ()=>{
+        addMessage(`You : ${value}`);
+    });
+    input.value = "";
+}
+
+function handleNameSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
+
+function showRoom(){
+    welcom.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNameSubmit);
+}
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
+    input.value = "";
+}
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} join `);
+});
+
+socket.on("bye", (left) => {
+    addMessage(`${left} left `);
+});
+
+socket.on("new_message", addMessage);
+
+form.addEventListener("submit", handleRoomSubmit);
