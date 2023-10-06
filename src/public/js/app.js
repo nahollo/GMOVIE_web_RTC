731d31cfa11a7f935954b24de205ac34c17a3781
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-10-07 01:33:31 +0900

    3.1
    good

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,11 +3,27 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
 
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        cameras.forEach((camera) => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
 
 async function getMedia() {
     try {
@@ -16,6 +32,7 @@ async function getMedia() {
             video: true,
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -23,24 +40,26 @@ async function getMedia() {
 
 getMedia();
 
-function handleMuteClick(){
-    if(!muted){
+function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+    if (!muted) {
         muteBtn.innerText = "음소거 　풀기"
         muted = true;
-    } else{
+    } else {
         muteBtn.innerText = "마이크 음소거"
         muted = false;
     }
 }
-function handleCameraClick(){
-    if(cameraOff){
+function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    if (cameraOff) {
         cameraBtn.innerText = "카메라 켜기"
         cameraOff = false;
-    } else{
+    } else {
         cameraBtn.innerText = "카메라 끄기"
         cameraOff = true;
     }
 }
 
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
+muteBtn.addEventListener("click", handleMuteClick); // 음소거
+cameraBtn.addEventListener("click", handleCameraClick); // 카메라
