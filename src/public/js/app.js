1ed6cb0c4ff995177eb2ba88dc7bf79d83ce4fff
NaHyeongJin <skgudwlsdlrj@gmail.com>
2023-10-07 01:14:02 +0900

    3.0
    yes

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,80 +1,46 @@
 const socket = io();
 
-const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
-const room = document.getElementById("room");
-room.hidden = true;
 
-let roomName;
+let myStream;
+let muted = false;
+let cameraOff = false;
 
-function addMessage(message){
-    const ul = room.querySelector("ul");
-    const li = document.createElement("li");
-    li.innerText = message;
-    ul.appendChild(li);
-}
-
-function handleMessageSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#msg input");
-    const value = input.value;
-    socket.emit("new_message", input.value, roomName, ()=>{
-        addMessage(`You : ${value}`);
-    });
-    input.value = "";
-}
-
-function handleNameSubmit(event){
-    event.preventDefault();
-    const input = room.querySelector("#name input");
-    socket.emit("nickname", input.value);
-}
-
-function showRoom(){
-    welcome.hidden = true;
-    room.hidden = false;
-    const h3 = room.querySelector("h3");
-    h3.innerText = `Room ${roomName}`;
-    const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
-    msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNameSubmit);
-}
-
-function handleRoomSubmit(event){
-    event.preventDefault();
-    const input = form.querySelector("input");
-    socket.emit("enter_room", input.value, showRoom);
-    roomName = input.value;
-    input.value = "";
-}
-
-socket.on("enter_room", (user) => {
-    addMessage(`${user} join `);
-});
-
-socket.on("welcome", (user) => {
-    addMessage(`${user} join `);
-});
-
-socket.on("bye", (left) => {
-    addMessage(`${left} left `);
-});
-
-socket.on("new_message", addMessage);
-
-socket.on("room_change", rooms => {
-    const roomList = welcome.querySelector("ul");
-    roomList.innerHTML = "";
-    if(rooms.length === 0){
-        return;
+async function getMedia() {
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+        myFace.srcObject = myStream;
+    } catch (e) {
+        console.log(e);
     }
-    rooms.forEach(room => {
-        const li = document.createElement("li");
-        li.innerText = room;
-        roomList.append(li);
-    });
-});
+}
 
-form.addEventListener("submit", handleRoomSubmit);
+getMedia();
+
+function handleMuteClick(){
+    if(!muted){
+        muteBtn.innerText = "음소거 　풀기"
+        muted = true;
+    } else{
+        muteBtn.innerText = "마이크 음소거"
+        muted = false;
+    }
+}
+function handleCameraClick(){
+    if(cameraOff){
+        cameraBtn.innerText = "카메라 켜기"
+        cameraOff = false;
+    } else{
+        cameraBtn.innerText = "카메라 끄기"
+        cameraOff = true;
+    }
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
--- src/server.js
+++ src/server.js
@@ -1,85 +1,19 @@
 import http from "http";
 // import WebSocket from "ws";
 import express from "express";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import { Socket } from "dgram";
+import { instrument } from "@socket.io/admin-ui"
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname+"/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get("/*",(req,res) => res.redirect("/"));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app); // http 서버 작동 // socket IO 는 websocket의 부가기능이 아니다.
+const wsServer = new Server(httpServer);
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-
-const httpServer = http.createServer(app); // http 서버 작동
-const wsServer = SocketIO(httpServer); // socket IO 는 websocket의 부가기능이 아니다.
-
-function publicRooms(){
-    const {sockets:{adapter: {sids, rooms}}} = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_,key)=>{
-        if(sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-
-wsServer.on("connection", (socket) => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event)=>{
-        console.log(`Socket Event : ${event}`);
-    });
-    socket.on("enter_room", (roomName, done)=> {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nickname);
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", ()=>{
-        socket.rooms.forEach(room=> socket.to(room).emit("bye", socket.nickname));
-    })
-    socket.on("disconnect", ()=>{
-        wsServer.sockets.emit("room_change", publicRooms());
-    })
-
-    socket.on("new_message", (msg, room, done)=>{
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    })
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
-
-
-// (http서버랑 webSocket 서버는 따로 만들어도 됨)
-// const wss = new WebSocket.Server({server}); // webSocket 서버 작동
-
-// function handleConnection(socket){
-//     console.log(socket);
-//     // server.js의 socket은 연결된 브라우저
-// }
-
-// const sockets = []; // 서로 다른 브라우저를 확인해주기 위해
-
-// wss.on("connection", (socket) =>{
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon"; // 접속됐을 때 Anon이 닉네임의 기본 값
-//     console.log("Connected to Browser");
-//     socket.on("close",() => console.log("Disconnected from Browser")); // 서버가 연결된 페이지가 닫혔을 때
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg);
-//         switch(message.type){
-//             case "new_message":
-//                 sockets.forEach(aSocket =>aSocket.send(`${socket.nickname}: ${message.payload}`)); // 이 코드로 다른 브라우저에서 메세지를 보내도 모든 브라우저에서 확인 가능
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-
-//         }
-//     });
-// });
-
 httpServer.listen(3000, handleListen);
